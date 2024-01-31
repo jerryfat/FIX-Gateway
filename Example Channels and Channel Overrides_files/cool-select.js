42c7e5bbba43a2c003492c824b0576b62bd28597
JerryFat <jerryfat@verizon.net>
2024-01-31 18:16:51 -0500

    removed old or unused files

--- Example Channels and Channel Overrides_files/cool-select.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-
-  $( "table" ).addClass( "table table-striped" );
-
-  $( "#language-select" ).click(function() {
-    $( "#language-list" ).slideToggle();
-    $("#arrow").toggleClass("rotate-arrow");
-  });
-});
